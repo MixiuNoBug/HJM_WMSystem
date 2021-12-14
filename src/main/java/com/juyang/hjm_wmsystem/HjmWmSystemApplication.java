07bf1eeca52b0cac2a0ea6fb166bd7da04dfa47e
juyang <96057683+MixiuNoBug@users.noreply.github.com>
2021-12-14 17:15:42 +0800

    纯净基板

---
+++ src/main/java/com/juyang/hjm_wmsystem/HjmWmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.juyang.hjm_wmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjmWmSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjmWmSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/juyang/hjm_wmsystem/HjmWmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juyang.hjm_wmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjmWmSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
