bfcb8cae0b25c54462825a96890b6e695987494e
juyang <jy929746999@163.com>
2022-01-31 20:28:47 +0800

    log4j2配置出版调整

--- Source/src/main/java/com/juyang/hjm_wmsystem/config/SpringSeucrityConfig.java
+++ Source/src/main/java/com/juyang/hjm_wmsystem/config/SpringSeucrityConfig.java
@@ -1,8 +1,12 @@
 package com.juyang.hjm_wmsystem.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
 
 /**
  * @author JuYang
@@ -10,11 +14,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class SpringSeucrityConfig extends WebSecurityConfigurerAdapter {
+    Logger logger = LoggerFactory.getLogger( SpringSeucrityConfig.class );
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        logger.info( "------自定义登录接口Start------" );
         //当用户需要登录的时候转到登陆界面
         //自定义登陆接口
         http.formLogin().loginProcessingUrl( "/user/login" ).and().authorizeRequests().anyRequest().authenticated();
+        logger.info( "------自定义登陆接口End------" );
 
     }
 }
